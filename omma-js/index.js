b23587752f8036971fba4157311c66cabe802af5
muryllohenriq <muryllo.henrique@hotmail.com>
2022-07-30 11:37:28 -0300

    att

--- omma-js/index.js
+++ omma-js/index.js
@@ -3,7 +3,12 @@ console.log(nomeEmpresa);
 
 const listaDeReceitas = [
 {
-    id: 1;
-    
+    id: 1,
+    titulo: 'Bolo',
+    dificuldade: 'moderada',
+    ingredientes: ['Blá', 'Blá', 'Blá', 'Blá', 'Blá'],
+    preparo: 'Assar',
+    link: 'youtube.com',
+    vegano: false
 }
 ]
