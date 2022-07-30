e22e60021e0f3253ec4950a665ef2e08e89751cc
muryllohenriq <muryllo.henrique@hotmail.com>
2022-07-30 11:54:58 -0300

    att

--- omma-js/index.js
+++ omma-js/index.js
@@ -2,13 +2,45 @@ const nomeEmpresa = "Omma";
 console.log(nomeEmpresa);
 
 const listaDeReceitas = [
-{
+  {
     id: 1,
-    titulo: 'Bolo',
-    dificuldade: 'moderada',
-    ingredientes: ['Blá', 'Blá', 'Blá', 'Blá', 'Blá'],
-    preparo: 'Assar',
-    link: 'youtube.com',
-    vegano: false
-}
-]
+    titulo: "Bolo",
+    dificuldade: "moderada",
+    ingredientes: ["Blá", "Blá", "Blá", "Blá", "Blá"],
+    preparo: "Assar",
+    link: "youtube.com",
+    vegano: false,
+  },
+];
+
+const cadastrarReceita = (
+  id,
+  titulo,
+  dificuldade,
+  ingredientes,
+  preparo,
+  link,
+  vegano
+) => {
+  const novaReceita = {
+    id,
+    titulo,
+    dificuldade,
+    ingredientes,
+    preparo,
+    link,
+    vegano,
+  };
+  listaDeReceitas.push(novaReceita);
+  console.log(`Cadastro da receita ${titulo} feito com sucesso!`);
+};
+cadastrarReceita(
+  2,
+  "Panqueca",
+  "simples",
+  ["Blá", "Blá", "Blá", "Blá", "Blá"],
+  "Fritar",
+  "youtube.com",
+  false
+);
+console.log(listaDeReceitas)
